fe61c55c43df6e9ee2cee293f342e8b5ad7ac3e1
ovalekseeva <ovalekseeva@ovalekseeva-HP>
2019-03-06 16:46:21 +0300

    1

---
+++ src/test22/GitExps.java
@@ -0,0 +1,24 @@
+package test22;
+
+import java.util.Random;
+import java.util.TreeSet;
+
+public class GitExps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		int a=rand.nextInt();
+		int b=rand.nextInt();
+		System.out.println(a);
+		System.out.println(b);
+		
+		TreeSet<Integer> treeSet = new TreeSet<Integer>();
+		treeSet.add(a);
+		treeSet.add(b);
+		
+		System.out.println("maxNumber is "+treeSet.last());
+
+	}
+
+}
